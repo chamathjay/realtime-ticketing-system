42801e2795ed829f554186310d55ad7fb6f71d2f
CJ <160167403+chamathjay@users.noreply.github.com>
2024-12-03 11:53:55 +0530

    Add option to load from config.json and make vendors and customer threads in a loop

---
+++ backend/src/main/java/com/chamathjay/realtime_ticketing_backend/RealtimeTicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chamathjay.realtime_ticketing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeTicketingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeTicketingBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/chamathjay/realtime_ticketing_backend/RealtimeTicketingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamathjay.realtime_ticketing_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeTicketingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
