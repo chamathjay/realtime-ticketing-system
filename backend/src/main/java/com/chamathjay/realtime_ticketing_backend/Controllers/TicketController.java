7e91cd8176ff98690767a7e921a71da2c75f25e0
CJ <160167403+chamathjay@users.noreply.github.com>
2024-12-09 14:43:04 +0530

    Revert "Change CLI ticket pool to use ReEntrantLock instead of synchronized blocks."
    This reverts commit 31e8bd7334ad02a44eb913065108453f2d39106e.

--- backend/src/main/java/com/chamathjay/realtime_ticketing_backend/Controllers/TicketController.java
+++ backend/src/main/java/com/chamathjay/realtime_ticketing_backend/Controllers/TicketController.java
@@ -1,39 +1,22 @@
 package com.chamathjay.realtime_ticketing_backend.Controllers;
 
-import com.chamathjay.realtime_ticketing_backend.Services.TicketService;
+import java.util.*;
+import com.chamathjay.realtime_ticketing_backend.Databases.TicketRepository;
+import com.chamathjay.realtime_ticketing_backend.Models.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
-@RequestMapping("/api/ticketing")
 public class TicketController {
-    private final TicketService ticketService;
-
     @Autowired
-    public TicketController(TicketService ticketService) {
-        this.ticketService = ticketService;
+    private TicketRepository ticketRepository;
+    public TicketController(TicketRepository ticketRepository) {}
+
+    @GetMapping
+    public List<Ticket> getAllTickets() {
+        return ticketRepository.findAll();
     }
 
-    @PostMapping("/start")
-    public String startSimulation() {
-        ticketService.startSimulation();
-        return "Simulation started.";
-    }
-
-    @PostMapping("/stop")
-    public String stopSimulation() {
-        ticketService.stopSimulation();
-        return "Simulation stopped.";
-    }
-
-    @GetMapping("/logs")
-    public List<String> getLogs() {
-        List<String> logs = ticketService.getLogs();
-        return (logs);
-    }
 }
