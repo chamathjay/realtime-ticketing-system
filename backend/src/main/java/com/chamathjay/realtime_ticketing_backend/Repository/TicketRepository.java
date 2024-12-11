cd5e1b0d5079fadf1cb2ef52eadef1279e5f97de
CJ <160167403+chamathjay@users.noreply.github.com>
2024-12-12 02:35:13 +0530

    remove unused files created for database

--- backend/src/main/java/com/chamathjay/realtime_ticketing_backend/Repository/TicketRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chamathjay.realtime_ticketing_backend.Repository;
-
-import com.chamathjay.realtime_ticketing_backend.Models.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-
-}
